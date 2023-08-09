f7c60e10ad29ef1050e7feadad1434c483f3c134
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-08-09 18:29:15 +0700

    feat: add vue-flow

--- src/views/CanvasView.vue
+++ src/views/CanvasView.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
 import dot from "@/assets/dot.svg";
-import Node from "@/components/Node.vue";
-import { useCanvas } from "@/composables/useCanvas";
 import { job } from "@/utils/sampleJob.ts";
-import { onMounted, ref } from "vue";
+import "@vue-flow/controls/dist/style.css";
+import { Element, VueFlow } from "@vue-flow/core";
+import { Controls } from "@vue-flow/controls";
+import { ref } from "vue";
 
 const jobRef = ref(job);
-const canvas = ref<Element>();
-const { init, nodesRef, els, addNode, deleteNode } = useCanvas(jobRef);
+const elements = ref<Element[]>([
+  { id: "1", type: "input", label: "Node 1", position: { x: 250, y: 5 } },
 
-onMounted(() => {
-  try {
-    init(canvas.value!);
-  } catch (error) {
-    console.error(error);
-  }
-});
+  // Default nodes, you can omit `type: 'default'`
+  { id: "2", label: "Node 2", position: { x: 100, y: 100 } },
+  { id: "3", label: "Node 3", position: { x: 400, y: 100 } },
+
+  // An output node, specified by using `type: 'output'`
+  { id: "4", type: "output", label: "Node 4", position: { x: 400, y: 200 } },
+
+  // Edges
+  // Most basic edge, only consists of an id, source-id and target-id
+  { id: "e1-3", source: "1", target: "3" },
+
+  // An animated edge
+  { id: "e1-2", source: "1", target: "2", animated: true },
+]);
 </script>
 
 <template>
@@ -29,16 +37,16 @@ onMounted(() => {
       <el-input v-model="jobRef.description" placeholder="Description" />
     </div>
     <el-row>
-      <el-button type="primary" @click="addNode()">Add node</el-button>
-      <el-button type="danger" @click="deleteNode()">Delete node</el-button>
+      <!-- <el-button type="primary" @click="addNode()">Add node</el-button>
+      <el-button type="danger" @click="deleteNode()">Delete node</el-button> -->
     </el-row>
-    <div class="grow bg-white bg-repeat bg-center overflow-hidden" :style="`background-image: url(${dot})`">
-      <div :class="`relative grow`" ref="canvas">
-        <Node :name="node.name" :id="node.id" v-for="node in els" ref="nodesRef" />
-      </div>
+    <div
+      class="grow bg-white bg-repeat bg-center overflow-hidden"
+      :style="`background-image: url(${dot})`"
+    >
+      <VueFlow fit-view-on-init v-model="elements"> <Controls /></VueFlow>
     </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
